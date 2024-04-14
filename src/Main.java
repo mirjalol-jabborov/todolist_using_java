90a0a4068f58db2a69cfef942113398a6f0814e4
Mirjalol Jabborov <jabborovmirjalol03@gmail.com>
2024-04-14 20:42:24 +0500

    Added ability to view, create and delete todos

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import Actions.Actions;
+import Util.Util;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    ArrayList<String> todolist = new ArrayList<>();
+
+    Util util = new Util();
+    Actions actions = new Actions();
+
+    while (true) {
+      util.printALine();
+
+      int userChoice = util.userChoice( "see the TodoList:", "create a new todo:", "delete a todo:");
+
+      switch (userChoice) {
+        case 1:
+          actions.viewTodoList(todolist);
+          break;
+        case 2:
+          todolist = actions.addNewTodo(todolist);
+          break;
+        case 3:
+          todolist = actions.deleteTodoWithIndex(todolist);
+          break;
+      }
+    }
+  }
+}
