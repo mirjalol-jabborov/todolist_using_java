1d2211934a83d17163d1c2587d24c3394aeb540d
rainworldlunatic <167628954+rainworldlunatic@users.noreply.github.com>
2024-04-21 15:36:31 -0400

    Update Main.java

--- src/main/java/org/todolist/Main.java
+++ src/main/java/org/todolist/Main.java
@@ -2,20 +2,23 @@ package org.todolist;
 
 import org.todolist.actions.Actions;
 import org.todolist.util.Util;
-
-import java.util.ArrayList;
+import java.util.ArrayList; 
+//import necessary packages
 
 public class Main {
   public static void main(String[] args) {
-    ArrayList<String> todolist = new ArrayList<>();
+    ArrayList<String> todolist = new ArrayList<>(); 
+    //create an array list of strings
 
     Util util = new Util();
     Actions actions = new Actions();
+    //creating objects of uil and action classes, for main menu
 
     while (true) {
       util.printALine();
       int userChoice = util.userChoice( "see the TodoList:", "create a new todo:", "delete a todo:");
-
+        //prompt user for choice
+      
       switch (userChoice) {
         case 1:
           actions.viewTodoList(todolist);
@@ -26,7 +29,7 @@ public class Main {
         case 3:
           todolist = actions.deleteTodoWithIndex(todolist);
           break;
-      }
+          }
     }
   }
 }
